06f4a6a8a3b1b75301c0de9e8fa5dbcace862c16
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2024-02-01 16:41:52 -0300

    feat: create form wrapper component

---
+++ src/components/FormWrapper.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState } from "react";
+import Create from "./Create";
+import Join from "./Join";
+
+export default function FormWrapper() {
+  const [selectedTab, setSelectedTab] = useState<"join" | "create">("join");
+
+  return (
+    <div className="w-full">
+      <div className="flex items-center text-center ">
+        <span
+          className={`w-1/2 p-4 rounded-t-md cursor-pointer ${
+            selectedTab === "join" ? "bg-dark-gray" : "text-cyan"
+          }`}
+          onClick={() => setSelectedTab("join")}
+        >
+          Ingressar
+        </span>
+        <span
+          className={`w-1/2 p-4 rounded-t-md cursor-pointer ${
+            selectedTab === "create" ? "bg-dark-gray" : "text-cyan"
+          }`}
+          onClick={() => setSelectedTab("create")}
+        >
+          Nova reunião
+        </span>
+      </div>
+
+      <div className="min-w-[500px] p-6 rounded-b-md space-y-6 px-10 bg-dark-gray">
+        {selectedTab === "join" ? <Join /> : <Create />}
+      </div>
+    </div>
+  );
+}
