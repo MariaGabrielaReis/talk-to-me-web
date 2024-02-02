dd49edc967a29e9919cffd93941fe1d907a31796
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2024-02-02 09:01:54 -0300

    feat: enable user cam

--- src/app/meeting/[id]/page.tsx
+++ src/app/meeting/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 import { useSocketContext } from "../../../contexts/SocketContext";
 
@@ -11,10 +11,21 @@ import Header from "@/components/Header";
 export default function Meeting({ params }: { params: { id: string } }) {
   const { socket } = useSocketContext();
 
+  const userCam = useRef<HTMLVideoElement>(null);
+
   useEffect(() => {
     socket?.on("connect", () => console.log("connected"));
     socket?.emit("subscribe", { meetingId: params.id, socketId: socket.id });
+    initCamera();
   }, [socket]);
+
+  async function initCamera() {
+    const video = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: { noiseSuppression: true, echoCancellation: true },
+    });
+    if (userCam.current) userCam.current.srcObject = video;
+  }
 
   return (
     <div className="h-screen">
@@ -23,7 +34,12 @@ export default function Meeting({ params }: { params: { id: string } }) {
       <div className="flex h-[75%] w-full max-w-[1400px] px-8 m-auto">
         <div className="h-full w-full m-3 grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="relative bg-gray w-full rounded-md p-2">
-            <video className="h-full w-full"></video>
+            <video
+              className="h-full w-full"
+              ref={userCam}
+              autoPlay
+              playsInline
+            />
             <span className="absolute bottom-2">Maby Reis</span>
           </div>
 
