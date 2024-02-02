04244106ed9a077ac33896553e5ec81876d08f07
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2024-02-02 08:50:57 -0300

    feat: create meeting

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -1,16 +1,32 @@
 "use client";
 
-import { useRef } from "react";
+import { FormEvent, useRef } from "react";
+
+import { useRouter } from "next/router";
 import Button from "./Button";
 import { Input } from "./Input";
 
 export default function Create() {
   const name = useRef<HTMLInputElement>(null);
+  const router = useRouter();
+
+  function handleCreateMeeting(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (name.current?.value) {
+      sessionStorage.setItem("username", name.current.value);
+      router.push(`/meeting/${generateRandomId()}`);
+    }
+  }
+
+  function generateRandomId() {
+    return Math.random().toString(36).substring(2, 7);
+  }
 
   return (
-    <>
+    <form onSubmit={handleCreateMeeting} className="space-y-6">
       <Input type="text" placeholder="Seu nome" ref={name} />
-      <Button title="Criar" />
-    </>
+      <Button type="submit" title="Criar" />
+    </form>
   );
 }
