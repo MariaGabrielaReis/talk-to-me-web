308730d2d443236d6d7ac3af48c0c0e153854b06
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2024-02-02 08:51:21 -0300

    feat: join in a meeting

--- src/components/Join.tsx
+++ src/components/Join.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useRef } from "react";
+import { FormEvent, useRef } from "react";
+
 import Button from "./Button";
 import { Input } from "./Input";
 
@@ -8,11 +9,20 @@ export default function Join() {
   const name = useRef<HTMLInputElement>(null);
   const meetingId = useRef<HTMLInputElement>(null);
 
+  function handleJoinMeeting(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (name.current?.value && meetingId.current?.value) {
+      sessionStorage.setItem("username", name.current.value);
+      window.location.href = `/meeting/${meetingId.current.value}`;
+    }
+  }
+
   return (
-    <>
+    <form onSubmit={handleJoinMeeting} className="space-y-6">
       <Input type="text" placeholder="Seu nome" ref={name} />
       <Input type="text" placeholder="ID da reunião" ref={meetingId} />
-      <Button title="Entrar" />
-    </>
+      <Button type="submit" title="Entrar" />
+    </form>
   );
 }
